a4edc5bf5b92bef6172925e047819e4c38126f8b
myguddy <seungil.han@gmail.com>
2020-04-01 21:51:59 +0900

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM g3.skb:8500/external-image/node:12
+FROM node:12.16-buster-slim
 
 RUN mkdir /app
 WORKDIR /app
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    endpoint :{
-        mongodb: "mongodb://localhost/berries",
-    }
-}
+	endpoint: {
+		mongodb: "mongodb://localhost/kubetv"
+	}
+};
